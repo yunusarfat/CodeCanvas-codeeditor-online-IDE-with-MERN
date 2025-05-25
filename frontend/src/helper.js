669f57313b3e978c4aef8c21d7b7a6472c5acc20
YunusMdArfat <140711122+yunusarfat@users.noreply.github.com>
2025-05-25 23:02:56 +0600

    Update helper.js

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -6,4 +6,6 @@ export const removeClass = (el, className) => {
   let elem = document.querySelector(el);
   elem.classList.remove(className);
 };
-export const api_base_url = "http://localhost:3000";
+
+export const api_base_url = process.env.REACT_APP_API_URL;
+
